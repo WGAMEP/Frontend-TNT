d55e9a315c0e41b0fcdb2d20ede016c729d8ad6f
WGAMEP <zzGAMEzz123@hotmail.com>
2023-07-23 14:21:32 +0700

    inint

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import About from './About'
+
+const data : string[] = ["a","b","c","d","ttt"]
+
+function App() {
+  //const [count, setCount] = useState(0)
+  const [state, setstate] = useState<string>("test");
+
+  return (
+      <div className='main'>
+        <h1>Hello react</h1>
+        <About data={data} Fuc = {setstate} />
+        <h1>{state}</h1>
+        <input onChange={(e) => setstate(state + e.target.value)} />
+        {/*{data.map((item,idx) => {
+          if(item ==="a") return <h1 key={idx}>AAA</h1>;
+          else return <h4 key={idx}>{item}</h4>
+        })}*/}
+      </div>
+  );
+}
+
+export default App
